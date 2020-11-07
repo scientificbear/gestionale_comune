232636a5973bd51e9d218eedaa54dfcc7b8994f6
Patrick Zecchin <scientific.bear@gmail.com>
2020-11-07 18:08:02 +0100

    Fix

--- html/general/utils.php
+++ html/general/utils.php
@@ -45,7 +45,6 @@ function protect_content($content, $this_role, $allowed_list){
     }
 
 function trunc_str($x, $max_l=50){
-    error_log($x);
     if (strlen($x)>$max_l){
         $x = substr($x, 0, $max_l);
         $x = $x . "...";
